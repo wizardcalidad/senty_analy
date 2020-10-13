45976525a0f0e3f4967f9f4613aa3a732df7f12f
Qoyum Yusuf <yusufqoyum01@gmail.com>
2020-10-13 16:34:46 +0100

    updating my requirements.txt

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,10 +1,11 @@
 import os
 from flask import Flask
 
+
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY = 'dev',
+        SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
     if test_config is None:
