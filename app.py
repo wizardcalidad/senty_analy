0b74d347845d3fafbd5027885922222defa41edd
Qoyum Yusuf <yusufqoyum01@gmail.com>
2020-10-13 15:53:55 +0100

    latest commit from __init__.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return 'Hello, Semicolon Devs'
-
-@app.route('/ping')
-def ping():
-    return 'pong!'
+# from flask import Flask
+#
+# app = Flask(__name__)
+#
+# @app.route('/')
+# def hello():
+#     return 'Hello, Semicolon Devs'
+#
+# @app.route('/ping')
+# def ping():
+#     return 'pong!'
