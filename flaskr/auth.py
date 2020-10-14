aced1ab9339987599c0aa73416e239ee6d787177
Qoyum Yusuf <yusufqoyum01@gmail.com>
2020-10-14 14:23:56 +0100

    All views completed

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -68,14 +68,25 @@ def load_logged_in_user():
     user_id = session.get('user_id')
 
     if user_id is None:
-        g.user =None
+        g.user = None
     else:
         g.user = get_db().execute(
             'SELECT * FROM user WHERE id = ?', (user_id,)
         ).fetchone()
+
 
 @bp.route('/logout')
 def logout():
     session.clear()
     return redirect(url_for('index'))
 
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
