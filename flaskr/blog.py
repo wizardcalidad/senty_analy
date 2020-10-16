b24d444f77dff6be4c50e3ead16bdcc34ce40385
Qoyum Yusuf <yusufqoyum01@gmail.com>
2020-10-16 12:43:54 +0100

    It's annoying but now it works

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -14,8 +14,8 @@ def index():
     db = get_db()
     posts = db.execute(
         'SELECT p.id, title, body, created, author_id, username'
-        'FROM post p JOIN user u ON p.author_id = u.id'
-        'ORDER BY created DESC'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
     ).fetchall()
     return render_template('blog/index.html', posts=posts)
 
@@ -48,8 +48,8 @@ def create():
 def get_post(id, check_author=True):
     post = get_db().execute(
         'SELECT p.id, title, body, created, author_id, username'
-        'FROM post p JOIN user u ON p.author_id = u.id'
-        'WHERE p.id = ?',
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' WHERE p.id = ?',
         (id,)
     ).fetchone()
 
