42c43139e96285dd4c9dba5b2a74ca1081bde5ed
Qoyum Yusuf <yusufqoyum01@gmail.com>
2020-10-16 15:14:45 +0100

    edited the auth.py

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -46,7 +46,7 @@ def login():
         db = get_db()
         error = None
         user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
+            ' SELECT * FROM user WHERE username = ?', (username,)
         ).fetchone()
         if user is None:
             error = 'Incorrect username.'
